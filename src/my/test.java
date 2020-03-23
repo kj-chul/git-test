9b80d97a6909b028ef2c8ab157a0b4d426ceba83
user2 <user2@DESKTOP-4HL1T3E>
2020-03-23 17:18:34 +0900

    hello world 수정
    hello world 내용 삭제 및 hello world3 추가

--- src/my/test.java
+++ src/my/test.java
@@ -2,7 +2,7 @@ package my;
 
 public class test {
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
 		System.out.println("HelloWorld2");
+		System.out.println("HelloWorld3");
 	}
 }
